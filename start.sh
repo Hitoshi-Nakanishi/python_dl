28205617a3464c039f3eb73090dfd15c0133f911
hitoshin <37>
2020-11-08 10:17:26 +0900

    cuda101_cudnn7_ubuntu1804_py38_torch170_vision081_tf220

--- start.sh
+++ start.sh
@@ -2,6 +2,12 @@
 docker run \
        -v /share2/hitoshin/DockerShared:/notebooks/DockerShared \
        --runtime=nvidia \
+<<<<<<< 4cf7c9090fcd8a811280e6baa7cc92aae9f9e730
        -p 16684:8888 -p 12684:6006 \
        --name hitoshi_container \
        -it x37n/python_dl-gpu:latest
+=======
+       -p 8370:8888 -p 8270:6006 \
+       --name hitoshi_py38 \
+       -it hitoshi/python-gpu:10.1-ubuntu18.04-F22T17
+>>>>>>> cuda101_cudnn7_ubuntu1804_py38_torch170_vision081_tf220
