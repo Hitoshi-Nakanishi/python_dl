17cad39b49687433c293f7b4e6c576fea8065ebf
Hitoshi <hitoshi.nakanishi12345@gmail.com>
2021-04-13 20:31:58 +0900

    example of pytorch docker

---
+++ backlogs/simple_example/pixyz_start.sh
@@ -0,0 +1,13 @@
+source activate py38
+pip install -e pjs/pixyz
+#pip install -e pjs/pixyzoo
+#tmux send-keys -t "sess:0.0" C-z 'bash pjs/ipynb.sh'
+#tmux select-pane -t "sess:0.0"
+#tmux select-window -t "sess:0"
+#tmux attach-session -t "sess"
+tmux
+
+
+#
+
+
