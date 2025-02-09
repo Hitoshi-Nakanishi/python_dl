c73c09923dd069635b871e4ffec171bb7716c62f
Hitoshi Nakanishi <hitoshi.nakanishi12345@gmail.com>
2025-02-09 22:19:30 +0900

    removed manual config

--- envs/ec/start_lily2.sh
+++
@@ -1,6 +0,0 @@
-# docker create jupyter notebook on tensorflow python3 and jupyter
-nvidia-docker run \
-       -v /share2/hitoshin/DockerShared:/notebooks/DockerShared \
-       -p 8390:8888 -p 8290:6006 \
-       --name hitoshi_py38 \
-       -it hitoshi/python-dl:10.1-ubuntu18.04-F22T17-v2
