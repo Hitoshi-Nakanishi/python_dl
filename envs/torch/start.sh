be0e6485b16f1cc7925477c2c1720594022f9c36
Hitoshi Nakanishi <hitoshi.nakanishi12345@gmail.com>
2025-02-11 16:42:21 +0900

    removed net host

--- envs/torch/start.sh
+++ envs/torch/start.sh
@@ -8,7 +8,7 @@ user_drive=/mnt/c/Users/hitos
 data_drive=/mnt/d
 log_dir=$data_drive/log
 IMAGE_TAG="mytorch:2.6.0-cuda12.6-cudnn9-runtime"
-# export DISPLAY=127.0.0.1:0.0
+export DISPLAY=127.0.0.1:0.0
 mkdir -p $log_dir
 
 docker run \
@@ -19,13 +19,13 @@ docker run \
   -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e LUID=$(id -u $USER) -e LGID=$(id -g $USER) \
+  -e DISPLAY=$DISPLAY \
   --device /dev/dri \
   --gpus 'all,"capabilities=compute,utility,graphics"' \
   -e NVIDIA_VISIBLE_DEVICES=all \
   -e NVIDIA_DRIVER_CAPABILITIES=all \
   -e __GLX_VENDOR_LIBRARY_NAME=nvidia \
   -e __NV_PRIME_RENDER_OFFLOAD=1 \
-  --network host \
   -p 8888:8888 \
   -p 6006:6006 \
   -p 5000:5000 \
@@ -34,6 +34,5 @@ docker run \
   -it $IMAGE_TAG \
   zsh
 
-# -e DISPLAY=$DISPLAY \
-
+# --network host \
 # -u $(id -u $USER):$(id -g $USER) \
